1771303c17f5b02467c269472c19653163d143c9
Dongwoon Jeong <dongwoon.dj@gmail.com>
2022-11-28 11:29:55 -0600

    model updated

--- src/com/dollarsbank/application/DollarsBankApplication.java
+++ src/com/dollarsbank/application/DollarsBankApplication.java
@@ -3,8 +3,7 @@ package com.dollarsbank.application;
 public class DollarsBankApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello123123");
+		System.out.println("program starting");
 	}
 
 }
--- src/com/dollarsbank/model/Account.java
+++ src/com/dollarsbank/model/Account.java
@@ -1,8 +1,97 @@
 package com.dollarsbank.model;
 
+import java.util.List;
+
 public class Account {
 
 	
 	private int acc_id;
+	private String name;
+	private String password;
+	private String address;
+	private String contact;
+	private List<String> history;
+	
+	
+
+
+	public Account(int acc_id, String name, String password, String address, String contact, List<String> history) {
+		super();
+		this.acc_id = acc_id;
+		this.name = name;
+		this.password = password;
+		this.address = address;
+		this.contact = contact;
+		this.history = history;
+	}
+
+
+	public int getAcc_id() {
+		return acc_id;
+	}
+
+
+	public void setAcc_id(int acc_id) {
+		this.acc_id = acc_id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getContact() {
+		return contact;
+	}
+
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+
+	public List<String> getHistory() {
+		return history;
+	}
+
+
+	public void setHistory(List<String> history) {
+		this.history = history;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Account [acc_id=" + acc_id + ", name=" + name + ", password=" + password + ", address=" + address
+				+ ", contact=" + contact + ", history=" + history + "]";
+	}
+	
+	
 	
 }
